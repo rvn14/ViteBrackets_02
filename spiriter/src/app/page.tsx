03a8d8683bfb1068ea23d072cd7cb5f7e07e73c9
rvn14 <dasunadithya123@gmail.com>
2025-03-09 13:51:05 +0530

    base UI

--- spiriter/src/app/page.tsx
+++ spiriter/src/app/page.tsx
@@ -33,14 +33,16 @@ export default function Home() {
   return (
 
     <div ref={refwrapper} className="w-full h-screen overflow-hidden">
-        <Navbar scrolldiv={refwrapper} />
+        <Navbar  />
     <main ref={refcontent} className="bg-[#000018] w-full flex flex-col items-center relative">
       <div className="fixed top-0 rounded-full w-1/2 h-[500px] bg-[#1789DC] blur-[150px] transform -translate-y-1/2 z-0"></div>
+      <img className="fixed top-0 w-full h-full object-center object-cover opacity-10 z-0" src="/images/batman.jpg" alt=""/>
       <div className="w-full h-full flex flex-col items-center z-10">
         
         <Hero />
-        <Hero />
-        <Hero />
+        <div className="w-full h-[2000px] flex flex-col items-center z-20">
+          
+        </div>
 
       </div>
     </main>
--- spiriter/src/components/Hero.tsx
+++ spiriter/src/components/Hero.tsx
@@ -2,10 +2,10 @@
 
 const Hero = () => {
   return (
-    <div className='w-full h-screen flex justify-center items-center'>
+    <div className='relative w-full h-screen flex justify-center items-center'>
         
-        <div className='w-full h-full flex justify-center items-center'>
-            
+        <div className='w-4/5 h-full flex justify-center items-center bg-center bg-cover'>
+            <div className="font-poppins text-7xl font-semibold text-white text-center">Unleash Your Cricket Passion on Spirit11!</div>
         </div>
     </div>
   )
